640a4d2ec306b0cc079308a11bd6c7d6af1af290
Jooyoung Son <julia.young0629@gmail.com>
2018-12-01 12:31:05 +0900

    build 폴더 commit

---
+++ precache-manifest.32627e52c3b1fd33ab093eb6e58a60dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b8361e12313c62f48b7a",
+    "url": "/static/js/main.b8361e12.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "b8361e12313c62f48b7a",
+    "url": "/static/css/main.b50541cf.chunk.css"
+  },
+  {
+    "revision": "a9da38329a90b89afc07b0efc52d9351",
+    "url": "/index.html"
+  }
+];
